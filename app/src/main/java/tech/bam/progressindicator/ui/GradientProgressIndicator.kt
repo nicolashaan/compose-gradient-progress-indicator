62414cd7bcb5ba797d15a3cdd144a6726a51d0f6
Nicolas Haan <nicolash@bam.tech>
2023-02-21 18:40:05 +0100

    step: raw import of CircularProgressIndicator

---
+++ app/src/main/java/tech/bam/progressindicator/ui/GradientProgressIndicator.kt
@@ -0,0 +1,70 @@
+package tech.bam.progressindicator.ui
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.progressSemantics
+import androidx.compose.material3.CircularIndicatorDiameter
+import androidx.compose.material3.drawCircularIndicator
+import androidx.compose.material3.drawDeterminateCircularIndicator
+import androidx.compose.material3.toColor
+import androidx.compose.material3.tokens.CircularProgressIndicatorTokens
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun CircularProgressIndicator(
+    progress: Float,
+    modifier: Modifier = Modifier,
+    color: Color = CircularProgressIndicatorTokens.ActiveIndicatorColor.toColor(),
+    strokeWidth: Dp = CircularProgressIndicatorTokens.ActiveIndicatorWidth
+) {
+    val stroke = with(LocalDensity.current) {
+        Stroke(width = strokeWidth.toPx(), cap = StrokeCap.Butt)
+    }
+    Canvas(
+        modifier
+            .progressSemantics(progress)
+            .size(CircularIndicatorDiameter)
+    ) {
+        // Start at 12 o'clock
+        val startAngle = 270f
+        val sweep = progress * 360f
+        drawDeterminateCircularIndicator(startAngle, sweep, color, stroke)
+    }
+}
+
+private fun DrawScope.drawDeterminateCircularIndicator(
+    startAngle: Float,
+    sweep: Float,
+    color: Color,
+    stroke: Stroke
+) = drawCircularIndicator(startAngle, sweep, color, stroke)
+
+private fun DrawScope.drawCircularIndicator(
+    startAngle: Float,
+    sweep: Float,
+    color: Color,
+    stroke: Stroke
+) {
+    // To draw this circle we need a rect with edges that line up with the midpoint of the stroke.
+    // To do this we need to remove half the stroke width from the total diameter for both sides.
+    val diameterOffset = stroke.width / 2
+    val arcDimen = size.width - 2 * diameterOffset
+    drawArc(
+        color = color,
+        startAngle = startAngle,
+        sweepAngle = sweep,
+        useCenter = false,
+        topLeft = Offset(diameterOffset, diameterOffset),
+        size = Size(arcDimen, arcDimen),
+        style = stroke
+    )
+}
